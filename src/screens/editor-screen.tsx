fa567af0450dc5ba9e5b596094199076f99dfced
Ole Tjernstad <ott556@gmail.com>
2022-06-22 11:55:00 +0200

    if no files exist navigate back to start

--- src/screens/editor-screen.tsx
+++ src/screens/editor-screen.tsx
@@ -1,8 +1,31 @@
+import { useEffect, useMemo } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+
 import Editor from "../components/editor";
 import { useFile } from "../context/file";
 
 export default function EditorScreen() {
-  const { createNewFile } = useFile();
+  const { createNewFile, files } = useFile();
+
+  const { id } = useParams();
+  const navigate = useNavigate();
+  console.log(id);
+
+  const file = useMemo(() => {
+    return files.find((f) => f.id === id);
+  }, [id, files]);
+
+  useEffect(() => {
+    const file = files.find((f) => f.id === id);
+
+    if (!file) {
+      navigate("/");
+    }
+  }, [files, id, navigate]);
+
+  if (!file) {
+    return <div />;
+  }
   return (
     <div
       style={{
